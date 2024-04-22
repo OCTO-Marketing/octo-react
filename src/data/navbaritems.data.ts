39fb3f1b8d201a01c2e44116f16f79fc7cfa6f1d
SMandadi9 <121359419+SMandadi9@users.noreply.github.com>
2024-04-22 13:14:08 -0700

    Adding teja's branch into jinnie's (#14)
    * feat: Implemented Careers Search Tools Page
    
    * feat:Tech Interview Prep Career Page
    
    * feat: Added Links
    
    * feat:Job Search Platforms
    
    * feat: Archived Apply
    
    * feat: Search Platforms Tree Diagram
    
    * feat: Search Platforms Tree Diagram (using react flow)
    
    * Add border radius
    
    * Changed Interview Pictures and Modified Layout
    
    * navbar
    
    * Changed Dropdown Opacity
    
    * Revert to 3 cols
    
    * Added Functional Arrow
    
    * Change Interview Page to 2 Grid
    
    * create toggle and 2nd tree diagram
    
    * corp eng page
    
    ---------
    
    Co-authored-by: Jared Basilio <Jaredbasilio02@gmail.com>
    Co-authored-by: joanneberk <123433211+joanneberk@users.noreply.github.com>
    Co-authored-by: Rachel Hu <rachelehu@gmail.com>
    Co-authored-by: Jared Basilio <40751861+JaredBasilio@users.noreply.github.com>

--- src/data/navbaritems.data.ts
+++ src/data/navbaritems.data.ts
@@ -4,6 +4,10 @@ import Projects from '../pages/Projects.page'
 import Team from '../pages/Team.page'
 import Research from '../pages/Research.page' //Import research page
 import ColdEmail from '../pages/ColdEmail.page' //Import research page
+import CorporateEng from 'pages/CorporateEng.page'
+import SearchTools from '../pages/Careers/SearchTools.page'
+import InterviewPrep from '../pages/Careers/InterviewPrep.page'
+import SearchPlatform from '../pages/Careers/SearchPlatform.page'
 
 export interface NavBarItem {
     path: string
@@ -11,15 +15,24 @@ export interface NavBarItem {
     unavailable?: boolean
     active?: boolean
     new?: boolean
-    component: () => JSX.Element
+    subItems?: NavBarItem[]
+    component?: () => JSX.Element
 }
+
+export const CAREERS_ITEMS: NavBarItem[] = [
+    { path: '/careers/searchtools', label: 'Search Tools', component: SearchTools },
+    { path: '/careers/interview', label: 'Interview Prep', component: InterviewPrep },
+    { path: '/careers/searchplatform', label: 'Search Platform', component: SearchPlatform },
+]
 
 export const NAV_BAR_ITEMS: NavBarItem[] = [
     { path: '/research', label: 'Research', component: Research }, //Add Research Page in NavBar
     { path: '/coldEmail', label: 'Research-ColdEmail', component: ColdEmail }, //Add Research Page in NavBar
+    { path: '/CorporateEng', label: 'Corporate Engineering', component: CorporateEng },
     // { path: '/case-comp', label: 'Case Competition', component: CaseComp },
     { path: '/team', label: 'Team', component: Team },
     { path: '/projects', label: 'Projects', component: Projects },
     // { path: '/apply', label: 'Apply', component: Apply },
     { path: '/api', label: 'API', component: APIDocs },
+    { path: '/careers', label: 'Careers', subItems: CAREERS_ITEMS},
 ]
---
+++ src/pages/Careers/SearchTools.page.tsx
@@ -0,0 +1,87 @@
+import { useNavigate } from 'react-router-dom'
+import { CenterAlignColumns, TwoToOneGrid, Columns } from '../../components/Layout/Columns.layout'
+import { WidePageLayout } from '../../components/Layout/Page.layout'
+import {VENTURE_CAPITAL_PORTFOLIOS, STANDARD_SEARCH_TOOLS, AREA_SPECIFIC_TOOLS} from '../../data/jobsearchtools'
+import type { TechJobSearchToolCard } from '../../data/jobsearchtools'
+import { TbLink } from 'react-icons/tb'
+
+const SearchTools = () => {
+    return (
+        <WidePageLayout>
+            <div>
+                <h1>Tech Job Search Tools</h1>
+                <h5 className='mt-4'>
+                Streamline your career internship, new grad, research roles and more using these convenient search tools                </h5>
+            </div>
+            <Resources resources={STANDARD_SEARCH_TOOLS}/>
+            <div>
+                <h1>Province/State Specific Company Lists</h1>
+                <h5 className='mt-4'>
+                Streamline your career internship, new grad, research roles and more using these convenient search tools                </h5>
+            </div>
+            <Resources resources={AREA_SPECIFIC_TOOLS}/>
+            <div>
+                <h1>Venture Capital Portfolios</h1>
+                <h5 className='mt-4'>
+                Search through top Venture Capital companies for potential job opportunities</h5>
+            </div>
+            <Resources resources={VENTURE_CAPITAL_PORTFOLIOS}/>
+        </WidePageLayout>
+    )
+}
+
+// Maps a list of resources to a two column table
+const Resources = ({resources}: {resources: TechJobSearchToolCard[]}) => (
+    <TwoToOneGrid>
+        {resources.map(({ tool_name, description, img_path, tool_link }) => (
+            <ResourceCard
+                key={tool_name}
+                tool_name={tool_name}
+                img_path={img_path}
+                description={description}
+                tool_link={tool_link}
+            />
+        ))}
+    </TwoToOneGrid>
+)
+
+// Card for each resource
+const ResourceCard = ({
+    tool_name,
+    img_path,
+    tool_link,
+    description
+}: TechJobSearchToolCard) => {
+    const navigate = useNavigate()
+    return (
+        <div className="p-6 bg-white rounded-xl shadow-card animate-showing duration-300 hover:shadow-card-hover ease-out">
+            <CenterAlignColumns>
+                <img
+                    src={img_path}
+                    alt={`team icon for ${tool_name}`}
+                    className="w-24 h-24 sm:w-16 sm:h-16 rounded-md"
+                />
+                <div className="w-auto overflow-auto">
+                    <h3 className="text-lg font-bold">{tool_name}</h3>
+                    <p className="mt-2 mb-1 text-sm leading-6 font-medium">
+                        {description}
+                    </p>
+                    <Columns wrapping>
+                        {tool_link &&
+                        <li
+                            key={tool_link?.label}
+                            className="text-accent list-none text-sm cursor-pointer whitespace-nowrap"
+                            onClick={() => {
+                                window.location.href = tool_link?.link
+                            }}
+                        >
+                            <TbLink className="inline" /> {tool_link?.label}
+                        </li>}
+                    </Columns>
+                </div>
+            </CenterAlignColumns>
+        </div>
+    )
+}
+
+export default SearchTools
