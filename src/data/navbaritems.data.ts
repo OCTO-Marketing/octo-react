53fea7aad3320feacb946169b24b0fcc51b882b3
SMandadi9 <smandadi@berkeley.edu>
2024-04-22 12:25:29 -0700

    corp eng page

--- src/data/navbaritems.data.ts
+++ src/data/navbaritems.data.ts
@@ -4,6 +4,7 @@ import Projects from '../pages/Projects.page'
 import Team from '../pages/Team.page'
 import Research from '../pages/Research.page' //Import research page
 import ColdEmail from '../pages/ColdEmail.page' //Import research page
+import CorporateEng from 'pages/CorporateEng.page'
 
 export interface NavBarItem {
     path: string
@@ -17,6 +18,7 @@ export interface NavBarItem {
 export const NAV_BAR_ITEMS: NavBarItem[] = [
     { path: '/research', label: 'Research', component: Research }, //Add Research Page in NavBar
     { path: '/coldEmail', label: 'Research-ColdEmail', component: ColdEmail }, //Add Research Page in NavBar
+    { path: '/CorporateEng', label: 'Corporate Engineering', component: CorporateEng },
     // { path: '/case-comp', label: 'Case Competition', component: CaseComp },
     { path: '/team', label: 'Team', component: Team },
     { path: '/projects', label: 'Projects', component: Projects },
