d59e316ea27ede04b8f72e630e2986b684c04c14
Qi Li Yang <qiyang2@berkeley.edu>
2025-05-01 13:19:51 -0700

    fix dropdown

--- src/components/Components/Dropdown.component.tsx
+++ src/components/Components/Dropdown.component.tsx
@@ -51,7 +51,7 @@ export const Dropdown = ({items, children}: {
                             const { path, label} = item
                             return (
                                 <li
-                                    className={`cursor-pointer block px-4 py-2 text-sm text-gray-400 hover:bg-gray-100 hover:text-gray-700 transition-opacity duration-300`}
+                                    className={`cursor-pointer block px-4 py-2 text-sm text-gray-400 hover:bg-gray-100 transition-opacity duration-300`}
                                     onClick={(e) => {
                                         navigate(path) 
                                         toggleDropdown();
