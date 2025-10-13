0904379d7eb2c777e2a977c05b2d51fc7771daf4
Rodrigo Ortiz <rodrigo.ortiz@berkeley.edu>
2025-10-12 17:12:27 -0700

    chore: add BearBites, MoffittStatus, and Decal teams

--- src/data/teams.ts
+++ src/data/teams.ts
@@ -4,6 +4,12 @@ import bm from '../assets/teams/bm.jpg'
 import bm_preview from '../assets/teams/bm_preview.png'
 import website from '../assets/teams/Website.png'
 import website_preview from '../assets/teams/Website_preview.png'
+import bearbites from '../assets/teams/bearbites.png'
+import bearbites_preview from '../assets/teams/bearbites_preview.png'
+import moffitstatus from '../assets/teams/moffitstatus.png'
+import moffitstatus_preview from '../assets/teams/moffitstatus_preview.png'
+import decal from '../assets/teams/decal.png'
+import decal_preview from '../assets/teams/decal_preview.png'
 
 export interface PTeamCard {
     team_name: string
@@ -12,7 +18,7 @@ export interface PTeamCard {
     app_links?: { position: string; link: string }[]
     website_link?: string
     descriptions?: string
-    preview_img_path?: string;
+    preview_img_path?: string
 }
 
 const Berkeleytime: PTeamCard = {
@@ -22,13 +28,13 @@ const Berkeleytime: PTeamCard = {
     app_links: [
         {
             position: 'Apply',
-            link: 'https://berkeleytime.com/apply',
-        },
+            link: 'https://berkeleytime.com/apply'
+        }
     ],
     website_link: 'https://berkeleytime.com',
     descriptions:
         "Contribute to the development of Berkeley's go-to platform for course discovery, grade distribution insights, and seamless schedule planning, enabling students to make informed academic choices with ease.",
-    preview_img_path: bt_preview,
+    preview_img_path: bt_preview
 }
 
 const BerkeleyMobile: PTeamCard = {
@@ -38,13 +44,13 @@ const BerkeleyMobile: PTeamCard = {
     app_links: [
         {
             position: 'Apply',
-            link: 'https://forms.gle/uARTVwHa7kNWQ5qp7',
-        },
+            link: 'https://forms.gle/uARTVwHa7kNWQ5qp7'
+        }
     ],
     website_link: 'https://forms.gle/uARTVwHa7kNWQ5qp7',
     descriptions:
         "Help develop Berkeley's student-created mobile app and collaborate with the campus community to implement cutting-edge features, including an interactive campus navigation map for efficient route planning, a comprehensive dining hall menu page, real-time safety alerts, and a convenient study page to locate the nearest open library.",
-    preview_img_path: bm_preview,
+    preview_img_path: bm_preview
 }
 
 const Website: PTeamCard = {
@@ -54,19 +60,70 @@ const Website: PTeamCard = {
     app_links: [
         {
             position: 'Apply',
-            link: 'https://forms.gle/bqE3ZcUum14Ltnxp8',
-        },
+            link: 'https://forms.gle/CkcT3nmbntkPv1RR6'
+        }
     ],
     website_link: 'https://octo.asuc.org',
     descriptions:
         'Assist the lead webmaster with maintaining, designing, and developing websites for both OCTO and the ASUC, ensuring that sites are visually appealing, user-friendly, and aligned with organizational goals.',
-    preview_img_path: website_preview,
+    preview_img_path: website_preview
+}
+
+const BearBites: PTeamCard = {
+    team_name: 'BearBites',
+    positions: '',
+    img_path: bearbites,
+    app_links: [
+        {
+            position: 'Apply',
+            link: ''
+        }
+    ],
+    website_link: '',
+    descriptions:
+        'Maintain and improve BearBites, a real-time notification platform that alerts students about free leftover food on campus, reducing waste, saving students money, and connecting them with campus communities through instant alerts and organization-level tagging.',
+    preview_img_path: bearbites_preview
+}
+
+const MoffitStatus: PTeamCard = {
+    team_name: 'MoffitStatus',
+    positions: '',
+    img_path: moffitstatus,
+    app_links: [
+        {
+            position: 'Apply',
+            link: ''
+        }
+    ],
+    website_link: 'https://www.instagram.com/moffittstatus',
+    descriptions:
+        "Contribute to the development of Berkeley's Library Crowd Meter platform, MoffittStatus, which provides real-time data on library occupancy and study space availability. Enhance the platform’s data collection, visualization, and user interface features to help students conveniently find open study areas and manage their time more efficiently during peak hours.",
+    preview_img_path: moffitstatus_preview
+}
+
+const Decal: PTeamCard = {
+    team_name: 'Decal',
+    positions: '',
+    img_path: decal,
+    app_links: [
+        {
+            position: 'Apply',
+            link: ''
+        }
+    ],
+    website_link: 'https://decal.studentorg.berkeley.edu',
+    descriptions:
+        "Contribute to the development of Berkeley's platform for DeCal course registration, discovery, and application. The website allows students to easily register for those DeCal courses by allowing them to be notified on time of open DeCals.",
+    preview_img_path: decal_preview
 }
 
 const TEAMS: PTeamCard[] = [
     Berkeleytime,
     BerkeleyMobile,
     Website,
+    BearBites,
+    MoffitStatus,
+    Decal
 ]
 
 export default TEAMS
--- src/pages/Projects.page.tsx
+++ src/pages/Projects.page.tsx
@@ -1,5 +1,8 @@
 import { useNavigate } from 'react-router-dom'
-import { CenterAlignColumns, LgColumn } from '../components/Layout/Columns.layout'
+import {
+    CenterAlignColumns,
+    LgColumn
+} from '../components/Layout/Columns.layout'
 import { WidePageLayout } from '../components/Layout/Page.layout'
 import type { PTeamCard } from '../data/teams'
 import TEAMS from '../data/teams'
@@ -9,60 +12,76 @@ const Projects = () => {
         <WidePageLayout>
             <div>
                 <h1>Projects</h1>
-                <h5 className='mt-4'>
+                <h5 className="mt-4">
                     Using the power of technology to help all Berkeley students
                 </h5>
             </div>
             <LgColumn>
-                {TEAMS.map(({ team_name, img_path, website_link, descriptions, preview_img_path }) => {
-                    return (
-                        <TeamCard
-                            key={team_name}
-                            team_name={team_name}
-                            website_link={website_link}
-                            img_path={img_path}
-                            descriptions={descriptions}
-                            preview_img_path={preview_img_path}
-                        />
-                    )
-                })}
+                {TEAMS.map(
+                    ({
+                        team_name,
+                        img_path,
+                        website_link,
+                        descriptions,
+                        preview_img_path
+                    }) => {
+                        return (
+                            <TeamCard
+                                key={team_name}
+                                team_name={team_name}
+                                website_link={website_link}
+                                img_path={img_path}
+                                descriptions={descriptions}
+                                preview_img_path={preview_img_path}
+                            />
+                        )
+                    }
+                )}
             </LgColumn>
         </WidePageLayout>
     )
 }
 
-const TeamCard = ({ team_name, img_path, website_link, descriptions, preview_img_path }: PTeamCard) => {
+const TeamCard = ({
+    team_name,
+    img_path,
+    website_link,
+    descriptions,
+    preview_img_path
+}: PTeamCard) => {
     const navigate = useNavigate()
     return (
-        <div className='flex'>
+        <div className="flex">
             <div
-                className='w-full px-8 py-6 bg-white rounded-xl shadow-card duration-300 hover:shadow-card-hover ease-out flex content-center animate-showing'
+                className="w-full px-8 py-6 bg-white rounded-xl shadow-card duration-300 hover:shadow-card-hover ease-out flex content-center animate-showing"
                 onClick={() => {
                     navigate('')
                 }}
             >
-                <div className='flex w-full'>
-                    <div className='w-2/3 flex content-center'>
+                <div className="flex w-full">
+                    <div className="w-2/3 flex content-center">
                         <CenterAlignColumns>
                             <img
                                 src={img_path}
                                 alt={`team icon for ${team_name}`}
-                                className='w-24 h-24 sm:w-16 sm:h-16 rounded-md'
+                                className="w-24 h-24 sm:w-16 sm:h-16 rounded-md"
                             />
                             <div>
-                                <h3 className='text-lg font-bold'>{team_name}</h3>
-                                <p className='mt-2 mb-1 text-sm leading-6 font-medium'>
+                                <h3 className="text-lg font-bold">
+                                    {team_name}
+                                </h3>
+                                <p className="mt-2 mb-1 text-sm leading-6 font-medium">
                                     {descriptions}
                                 </p>
                             </div>
                         </CenterAlignColumns>
                     </div>
-                    <div className='w-1/3 flex items-center pl-10 pr-3'>
+                    <div className="w-1/3 flex items-center pl-10 pr-3">
                         <a href={`${website_link}`}>
                             <img
                                 src={preview_img_path}
                                 alt={`website preview for ${team_name}`}
-                                className='w-full h-auto rounded-md transition-transform duration-300 ease-out transform hover:scale-120'
+                                className="w-full h-auto rounded-md transition-transform duration-300 ease-out transform hover:scale-120"
                             />
                         </a>
                     </div>
